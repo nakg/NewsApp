90fcdc176439ab43bccd792aa2c308e739f31bd3
김낙진 <nakjin@mail.shinhan.dev>
2022-02-05 17:16:05 +0900

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 60067670 on 2022/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
