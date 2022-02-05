a5d4aa9fef724b4b0dc96d6b84651c6d15a60fdd
김낙진 <nakjin@mail.shinhan.dev>
2022-02-05 22:01:15 +0900

    first commit

---
+++ NewsApp/Models/News.swift
@@ -0,0 +1,20 @@
+//
+//  News.swift
+//  NewsApp
+//
+//  Created by 60067670 on 2022/02/05.
+//
+
+import SwiftUI
+
+struct News: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct News_Previews: PreviewProvider {
+    static var previews: some View {
+        News()
+    }
+}
---
+++ NewsApp/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  NewsApp
+//
+//  Created by 60067670 on 2022/02/05.
+//
+
+import Foundation
